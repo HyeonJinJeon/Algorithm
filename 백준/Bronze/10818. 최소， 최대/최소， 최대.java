6b6d0a5e268e1cfd2f5d5754923078a64230e250
Hyeonjin Jeon <40857551+wjsguswls@users.noreply.github.com>
2022-10-08 14:26:54 +0900

    [Bronze III] Title: 최소, 최대, Time: 1672 ms, Memory: 318424 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,23 +1,31 @@
-
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub	
+		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int []arr = new int[N];
-		
 		for(int i=0; i<N; i++) {
 			arr[i] = sc.nextInt();
 		}
+
+		int max = arr[0], min = arr[0];
 		
-		Arrays.sort(arr);
+		for(int i=0; i<N; i++) {
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+			if(min > arr[i]) {
+				min = arr[i];
+			}
+		}
 		
-		System.out.printf("%d %d", arr[0], arr[N-1]);
+		System.out.printf("%d %d", min, max);
 
 	}
 
 }
+
