c0e0bce256c01966c39bc33fac567fdcd8782341
h_jinnny <40857551+HyeonJinJeon@users.noreply.github.com>
2024-03-28 00:52:08 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.35 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0; // 최빈값
+        int count1 = 1; // 현재 값 나온 횟수
+        int count2 = 0; // 최대 횟수
+        Arrays.sort(array);
+        if(array.length == 1){ // 하나만 있으면 그 값 출력
+            answer = array[0];
+        }else if(array.length == 2){
+            answer = -1;
+        }else{
+            for(int i =0; i<array.length-1;i++){
+                if (array[i] == array[i+1]){
+                    count1++; // 인덱스 i번째 i+1번째 같으면 count1 ++
+                }else{
+                    if(count1 > count2){
+                        count2 = count1; // 최대횟수 갱신
+                        count1 = 1; // count1 초기화
+                        answer = array[i]; // 최빈값 지정
+                    }else if(count2 > count1){
+                        count1 = 1;
+                    }else if (count2 == count1) { 
+                        count1 = 1;
+                        answer = -1;
+                    }
+                }
+            }
+            
+            }if(count1 > count2){
+                answer = array[array.length-1];
+            }else if(count2 == count1){ // {1,1,2,2}처럼 반복문 끝나서 위에 else if 문이 안돌아갈떄 대비
+                answer = -1;
+        }
+        return answer;
+    }
+}
