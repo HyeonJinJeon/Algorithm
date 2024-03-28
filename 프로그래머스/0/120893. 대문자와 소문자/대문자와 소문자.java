3d5c21da299113dc0391eddfa120e037d18f1252
h_jinnny <40857551+HyeonJinJeon@users.noreply.github.com>
2024-03-29 00:34:16 +0900

    [level 0] Title: 대문자와 소문자, Time: 16.09 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,11 +1,14 @@
+//String 클래스의 toLowerCase()와 toUpperCase()룰 사용할 수도 있다.
 class Solution {
     public String solution(String my_string) {
         String answer = "";
         for(int i = 0; i < my_string.length(); i++){
             if(my_string.charAt(i) >= 65 && my_string.charAt(i) <=90){
-                answer = answer + (my_string.charAt(i)+"").toLowerCase();
+                //answer = answer + (my_string.charAt(i)+"").toLowerCase();
+                answer = answer + (char)(my_string.charAt(i)+32);
             } else if(my_string.charAt(i) >= 97 && my_string.charAt(i) <=122){
-                answer = answer + (my_string.charAt(i)+"").toUpperCase();
+                //answer = answer + (my_string.charAt(i)+"").toUpperCase();
+                answer = answer + (char)(my_string.charAt(i)-32);
             }
         }
         return answer;
