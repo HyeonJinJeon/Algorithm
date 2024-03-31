b6f6684ecb7374144d36e3af63fcda93b2947c3d
h_jinnny <40857551+HyeonJinJeon@users.noreply.github.com>
2024-03-31 20:03:36 +0900

    [level 0] Title: 중복된 문자 제거, Time: 7.22 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -15,6 +15,19 @@ class Solution {
         for(int i = 0; i < arr.length; i++){
             answer = answer + arr[i];
         }
+        
+        /*
+        더 좋은 방법!!
+        indexOf()를 사용하기
+            -> indexOf()는 해당 문자열이 처음 나타나는 인덱스를 반환하는 것임
+                즉, 해당 문자열이 처음 나타나는 인덱스의 문자열만 추가함으로써 
+                뒤에 반복되서 나오는 문자열은 answer에 추가하지 않는다.
+        */ 
+        // for (int i = 0; i < my_string.length(); i++) {
+        //     if(my_string.indexOf(my_string.charAt(i)) == i) {
+        //         answer += my_string.charAt(i);
+        //     }
+        // }
         return answer;
     }
 }
