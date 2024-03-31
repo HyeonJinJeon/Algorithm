940d1bc213816923add8ffba9048a3f205a56052
h_jinnny <40857551+HyeonJinJeon@users.noreply.github.com>
2024-03-31 18:23:57 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        
+        while(order != 0){
+            if((order%10) != 0){
+                if((order%10)%3 == 0){
+                    answer++;
+                    order /= 10;
+                }else {
+                    order /= 10;
+                }
+            }else {
+                order /= 10;
+            }
+        }
+        return answer;
+    }
+}
