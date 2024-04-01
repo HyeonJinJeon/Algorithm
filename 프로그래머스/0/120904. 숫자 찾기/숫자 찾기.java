2969eef875eddadca433ef159c63b383662a122d
h_jinnny <40857551+HyeonJinJeon@users.noreply.github.com>
2024-04-01 16:51:59 +0900

    [level 0] Title: 숫자 찾기, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,22 +1,46 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = 0;
-        int n = 0;
+        int answer = -1;
+        int result = -1;
+        int n = num;
         int length = 0;
         int cnt = 0;
         while(n > 0){
-            n = num/10;
+            n = n/10;
             length++;
         }
         while(num>0){
             cnt++;
             if(num%10 == k){
-                answer = length-cnt+2;    
-            }else{
-                answer = -1;
+                result = length-cnt+1;
             }
             num /= 10;
         }
+        if(result != -1) {
+            answer = result;
+        }
+
         return answer;
     }
 }
+/* 형변환을 해서 푸는 방법 */
+
+// class Solution {
+//     public int solution(int num, int k) {
+//         int answer = -1;
+//         String str = String.valueOf(num);
+//         char[] arr = new char[str.length()] ;
+
+//         for(int i=0;i<str.length();i++){
+//            arr[i] = str.charAt(i);
+//         }
+//         for(int i=0;i<arr.length;i++){
+//             if((int)arr[i]-'0'==k){
+//                 answer=i+1;
+//                 break;
+//             }
+//         }
+
+//         return answer;
+//     }
+// }
