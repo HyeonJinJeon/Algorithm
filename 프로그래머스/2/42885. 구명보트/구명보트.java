0521952db7adf7d60ee529e9e07fee5f5876aab9
h_jinnny <40857551+HyeonJinJeon@users.noreply.github.com>
2024-06-16 02:59:37 +0900

    [level 2] Title: 구명보트, Time: 8.24 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,22 +1,6 @@
-import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
-        Arrays.sort(people);
         int answer = 0;
-        int together = people.length-1;
-        for(int i = 0; i < people.length; i++){
-            if(i > together){
-                break;
-            }else if(i == together){
-                answer++;
-                break;
-            }
-            if(people[i] + people[together] > limit){
-                i--;
-            }
-            together--;
-            answer++;
-        }
         return answer;
     }
 }
