833b804aa51f2b1a2187f66a020395c6f664bd7b
h_jinnny <40857551+HyeonJinJeon@users.noreply.github.com>
2024-09-18 15:31:27 +0900

    [level 2] Title: [3차] 압축, Time: 20.04 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String msg) {
+        List<Integer> result = new ArrayList<>();
+        
+        // A~Z 색인 번호 초기화
+        Map<String, Integer> dictionary = new HashMap<>();
+        for (int i = 1; i <= 26; i++) {
+            dictionary.put(String.valueOf((char)(i + 64)), i);
+        }
+        
+        int dictIndex = 27;
+        int i = 0;
+        
+        while (i < msg.length()) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(msg.charAt(i));
+            int j = i + 1;
+            
+            // 색인번호 등록된 가장 긴 문자열 찾기
+            while (j < msg.length() && dictionary.containsKey(sb.toString() + msg.charAt(j))) {
+                sb.append(msg.charAt(j));
+                j++;
+            }
+            
+            // 현재 문자열에 해당하는 색인 번호를 결과에 추가
+            result.add(dictionary.get(sb.toString()));
+            
+            // 다음 문자까지 포함한 새로운 문자열을 사전에 추가
+            if (j < msg.length()) {
+                dictionary.put(sb.toString() + msg.charAt(j), dictIndex++);
+            }
+            
+            i = j;
+        }
+        
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
