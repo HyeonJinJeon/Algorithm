5d28794983bfaf8e0a9c4afe0854ae38b9a1c4db
h_jinnny <40857551+HyeonJinJeon@users.noreply.github.com>
2024-09-23 17:45:52 +0900

    [level 2] Title: [3차] 압축, Time: 27.90 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/README.md
+++ 프로그래머스/2/17684. ［3차］ 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 압축 - 17684 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684) 
 
 ### 성능 요약
 
-메모리: 79.8 MB, 시간: 20.04 ms
+메모리: 74.7 MB, 시간: 27.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 18일 15:31:23
+2024년 09월 23일 17:45:50
 
 ### 문제 설명
 
--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -5,9 +5,9 @@ class Solution {
         List<Integer> result = new ArrayList<>();
         
         // A~Z 색인 번호 초기화
-        Map<String, Integer> dictionary = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 1; i <= 26; i++) {
-            dictionary.put(String.valueOf((char)(i + 64)), i);
+            map.put(String.valueOf((char)(i + 64)), i);
         }
         
         int dictIndex = 27;
@@ -19,17 +19,17 @@ class Solution {
             int j = i + 1;
             
             // 색인번호 등록된 가장 긴 문자열 찾기
-            while (j < msg.length() && dictionary.containsKey(sb.toString() + msg.charAt(j))) {
+            while (j < msg.length() && map.containsKey(sb.toString() + msg.charAt(j))) {
                 sb.append(msg.charAt(j));
                 j++;
             }
             
             // 현재 문자열에 해당하는 색인 번호를 결과에 추가
-            result.add(dictionary.get(sb.toString()));
+            result.add(map.get(sb.toString()));
             
             // 다음 문자까지 포함한 새로운 문자열을 사전에 추가
             if (j < msg.length()) {
-                dictionary.put(sb.toString() + msg.charAt(j), dictIndex++);
+                map.put(sb.toString() + msg.charAt(j), dictIndex++);
             }
             
             i = j;
