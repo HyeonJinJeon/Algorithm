4b3131e053f16a988e8083e0512b338e1f6e9ee4
h_jinnny <40857551+HyeonJinJeon@users.noreply.github.com>
2025-08-20 23:15:11 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.11 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,29 +2,33 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
-        List<String> list = new ArrayList<>();
         
-        list.add(words[0]);
-        for(int i = 1; i < words.length; i++){
-            String preWord = list.get(i-1);
-            int wordLen = preWord.length();
-            char lastChar = preWord.charAt(wordLen-1);
-            char startChar = words[i].charAt(0);
-            
-            if(lastChar != startChar){
-                answer[0] = i % n + 1;
-                answer[1] = i / n + 1;
-                break;
+        int turn = 0;
+        int pNum = 0;
+        Map<String, Integer> memory = new HashMap<>();
+        for(int i = 0; i < words.length; i++) {
+            if(memory.get(words[i]) == null){
+                memory.put(words[i], 1);
+            }else {
+                memory.put(words[i], memory.get(words[i]) + 1);
             }
             
-            if(list.contains(words[i])){
-                answer[0] = i % n + 1;
-                answer[1] = i / n + 1;
+            if(memory.get(words[i]) > 1 || words[i].length() == 1 ){
+                turn = i / n + 1;
+                pNum = i % n + 1;
                 break;
             }
-            
-            list.add(words[i]);
+            if(i != 0){
+                if(words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)){
+                    turn = i / n + 1;
+                    pNum = i % n + 1;
+                    break;
+                }
+            }
         }
+        answer[0] = pNum;
+        answer[1] = turn;
+
         return answer;
     }
 }
