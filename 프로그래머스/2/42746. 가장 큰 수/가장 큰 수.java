bf37bf451e19a56edd35a35b8ab2f80d7d2197a3
h_jinnny <40857551+HyeonJinJeon@users.noreply.github.com>
2025-09-18 21:40:52 +0900

    [level 2] Title: 가장 큰 수, Time: 2869.08 ms, Memory: 395 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,26 +1,18 @@
 import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
-        // 숫자 배열을 문자열 배열로 변환
-        String[] strNumbers = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            strNumbers[i] = String.valueOf(numbers[i]);
+        String answer = "";
+        String[] strArr = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++){
+            strArr[i] = String.valueOf(numbers[i]);
         }
-
-        // 사용자 정의 정렬 기준을 사용하여 정렬
-        Arrays.sort(strNumbers, (a, b) -> (b + a).compareTo(a + b));
-
-        // 모든 숫자가 0인 경우를 처리
-        if (strNumbers[0].equals("0")) {
-            return "0";
+        Arrays.sort(strArr, (a, b) -> (b + a).compareTo(a + b));
+        for(int i = 0; i < strArr.length; i++){
+            answer += strArr[i];
         }
-
-        // 정렬된 문자열을 하나의 문자열로 합침
-        StringBuilder answer = new StringBuilder();
-        for (String num : strNumbers) {
-            answer.append(num);
+        if(strArr[0].equals("0")){
+            answer = "0";
         }
-
-        return answer.toString();
+        return answer;
     }
 }
