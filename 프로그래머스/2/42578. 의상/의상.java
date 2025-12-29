56ba820f9995723108b68d2ce9ab0341aeba5c8c
h_jinnny <40857551+HyeonJinJeon@users.noreply.github.com>
2025-12-29 21:35:55 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,16 +1,14 @@
 import java.util.*;
-
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
+        int answer = 0;
         Map<String, Integer> map = new HashMap<>();
-        for(String[] c : clothes) {
-            map.put(c[1], map.getOrDefault(c[1], 1) + 1);
+        for(int i = 0; i < clothes.length; i++){
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0)+1);
         }
-        for(int v : map.values()){
-            answer *= v;
+        for(int i : map.values()){
+            answer = answer + i + (answer * i);
         }
-        
-        return answer-1;
+        return answer;
     }
 }
